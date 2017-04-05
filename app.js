063fdc466d223146333f9c349ba2f52f1e35212f
eunchul <shdmscjf2@gmail.com>
2017-04-05 14:27:52 +0900

    mongodb 1

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
  */
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = require('./url');
 
 var app = express();
 
@@ -13,4 +16,14 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.listen(3000);
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+MongoClient.connect(url.mongodb, function(err, db) {
+    console.log(err);
+    //assert.equal(null, err);
+    console.log("Connected correctly to server");
+
+    db.close();
+});
